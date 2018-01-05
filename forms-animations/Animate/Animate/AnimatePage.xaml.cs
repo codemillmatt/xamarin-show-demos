c8f7a8a65447ef39805db27e2db71d221380fc88
Matt Soucoup <msoucoup@codemilltech.com>
2018-01-05 17:01:45 -0600

    Animation add

---
+++ forms-animations/Animate/Animate/AnimatePage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Animate
+{
+    public partial class AnimatePage : ContentPage
+    {
+        public AnimatePage()
+        {
+            InitializeComponent();
+        }
+
+        async void Translate_Clicked(object sender, System.EventArgs e)
+        {
+            await translateButton.TranslateTo(0, -100);
+            await translateButton.TranslateTo(100, -100);
+            await translateButton.TranslateTo(100, 0);
+            await translateButton.TranslateTo(0, 0);
+        }
+
+        async void Scale_Clicked(object sender, System.EventArgs e)
+        {
+            await scaleButton.ScaleTo(2);
+            await scaleButton.ScaleTo(1);
+        }
+
+        async void Rotate_Clicked(object sender, System.EventArgs e)
+        {
+            await rotateButton.RotateTo(180);
+            await rotateButton.RotateTo(0, 500, easing: Easing.BounceOut);
+        }
+
+        async void Fade_Clicked(object sender, System.EventArgs e)
+        {
+            await fadeButton.FadeTo(0, 500);
+            await fadeButton.FadeTo(1, 500);
+        }
+    }
+}
